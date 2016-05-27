e6edf8f4a857f43909a8c862652b6e2a99226c00
Fab F <MXO-GFX@Maximilians-iMac-2.local>
2016-05-27 17:31:15 +0200

    Transparency also fixed for 2D

--- 2D/Assets/Scripts/PlayPause.cs
+++ 2D/Assets/Scripts/PlayPause.cs
@@ -34,6 +34,7 @@ public class PlayPause : MonoBehaviour {
 				scrMedia.Pause();
 				Debug.Log ("pause");
 
+				play_btn.SetActive (true);
 				StartCoroutine (FadeTo (1.0f, 0.3f));
 				isPlaying = false;
 			}
@@ -51,11 +52,24 @@ public class PlayPause : MonoBehaviour {
 	// Fade-Animation des Alpha-Kanals
 	IEnumerator FadeTo(float aValue, float aTime){
 		float alpha = play_btn.GetComponent<Renderer> ().material.color.a;
+		bool isfadingOut = false;
+		if (alpha > 0.5f) 
+			isfadingOut = true;
+
 		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
 		{
 			Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha,aValue,t));
 			play_btn.GetComponent<Renderer> ().material.color = newColor;
+
+			// Lerp does never reach 1 or 0, so the button will be disabled when opacity is almost 0
+			if (isfadingOut == true && play_btn.GetComponent<Renderer> ().material.color.a < 0.1f) {
+				play_btn.SetActive (false);
+				isfadingOut = false;
+			}
+
 			yield return null;
 		}
 	}
 }
+
+
